b7a11ea11e9190b42ff0322aa047d88d12342fa2
Surender Venkatesan <89645185+Edmonstone@users.noreply.github.com>
2024-02-27 20:45:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -3,17 +3,24 @@ public:
     string removeDigit(string number, char digit) {
         string ans = "";
         int size = number.size();
-        for(int i=0;i < size ;i++){
-            if(number[i] == digit){
-                string temp = number.substr(0,i) + number.substr(i+1,size);
-                if(ans.empty()){
-                    ans = temp;
-                }
-                else{
-                    if(temp > ans) ans = temp;
+        
+        // Initialize index to -1 to represent no digit encountered yet
+        int maxDigitIndex = -1;
+
+        for (int i = 0; i < size; i++) {
+            if (number[i] == digit) {
+                // Update maxDigitIndex to the current index if it is greater
+                if (maxDigitIndex == -1 || number[i] > number[maxDigitIndex]) {
+                    maxDigitIndex = i;
                 }
             }
         }
+
+        // Remove the maximum digit encountered
+        if (maxDigitIndex != -1) {
+            ans = number.substr(0, maxDigitIndex) + number.substr(maxDigitIndex + 1, size - maxDigitIndex - 1);
+        }
+
         return ans;
     }
 };
