dbdef4712578aed60108fd156ad5a7d6f975a037
Surender Venkatesan <89645185+Edmonstone@users.noreply.github.com>
2024-02-28 22:05:12 +0530

    Time: 4 ms (95.18%), Space: 14.2 MB (8.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++){
-            int rem = target - nums[i];
-            if(m.find(rem) == m.end()) m[nums[i]] = i;
-            else return {m[rem],i};
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            int remainder = target - nums[i];
+            if (mp.find(remainder) != mp.end()) {
+                return {mp[remainder], i};
+            }
+            mp[nums[i]] = i;
         }
-        return {-1,-1};
+        return {-1, -1};
     }
 };
